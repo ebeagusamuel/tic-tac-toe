7bb222273a04c137448b3b897e6b935bbad395c0
ebeagusamuel <samuelebeagu@gmail.com>
2020-05-15 15:23:32 +0100

    Add methods to get players input

--- bin/main.rb
+++ bin/main.rb
@@ -3,31 +3,44 @@ def welcome
   puts 'Welcome to the Tic Tac Toe Game'
 end
 
-puts 'Player 1, please choose your symbol to use in the game...'
-PLAYER_ONE_SYMBOL = gets.chomp
-puts "Player 1 will use #{PLAYER_ONE_SYMBOL}"
-
-puts 'Player 2, please choose your symbol to use in the game...'
-PLAYER_TWO_SYMBOL = gets.chomp
-puts "Player 2 will use #{PLAYER_TWO_SYMBOL}"
-
-board_array = %w[1 2 3 4 5 6 7 8 9]
-
-def display_board(board_array)
-  puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
-  puts '-----------'
-  puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
-  puts '-----------'
-  puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
+def get_player1_symbol(symbol = nil)
+  puts 'Player 1, please choose your symbol to use in the game...'
+  player_one_symbol = gets.chomp
+  puts "Player 1 will use #{player_one_symbol}"
 end
 
-display_board(board_array)
+def get_player2_symbol(player_one_symbol)
+  puts 'Player 2, please choose your symbol to use in the game...'
+  player_two_symbol = gets.chomp
+  if player_one_symbol == player_two_symbol
+    puts 'Symbol chosen already, please chose another symbol'
+    player_two_symbol = gets.chomp 
+    
+    puts "Player 2 will use #{player_two_symbol}" if !player_one_symbol 
+  else
+    puts "Player 2 will use #{player_two_symbol}"
+  end
+end
 
-puts 'Game begins'
-puts 'Player 1, your turn'
-puts 'Player 2, your turn'
+# board_array = %w[1 2 3 4 5 6 7 8 9]
 
-puts 'Player 1 wins'
-puts 'Player 2 wins'
+# def display_board(board_array)
+#   puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
+#   puts '-----------'
+#   puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
+#   puts '-----------'
+#   puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
+# end
 
-puts 'Game ends'
+# display_board(board_array)
+
+# puts 'Game begins'
+# puts 'Player 1, your turn'
+# puts 'Player 2, your turn'
+
+# puts 'Player 1 wins'
+# puts 'Player 2 wins'
+
+# puts 'Game ends'
+welcome
+get_player_symbol
