decbe3855c19e741d9ebac329cf9e4e2a1171a97
Christopher Amanor <kriskit7@gmail.com>
2020-05-15 12:58:14 +0000

    Add player input requests and display_board method

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,21 @@
 #!/usr/bin/env ruby
-puts 'Hello World!'
+puts 'Welcome to the Tic Tac Toe Game'
+puts 'Player 1, please choose your symbol to use in the game...'
+PLAYER_ONE_SYMBOL = gets.chomp
+puts "Player 1 will use #{PLAYER_ONE_SYMBOL}"
+
+puts 'Player 2, please choose your symbol to use in the game...'
+PLAYER_TWO_SYMBOL = gets.chomp
+puts "Player 2 will use #{PLAYER_TWO_SYMBOL}"
+
+board_array = %w[1 2 3 4 5 6 7 8 9]
+
+def display_board(board_array)
+  puts " #{board_array[0]} | #{board_array[1]} | #{board_array[2]} "
+  puts '-----------'
+  puts " #{board_array[3]} | #{board_array[4]} | #{board_array[5]} "
+  puts '-----------'
+  puts " #{board_array[6]} | #{board_array[7]} | #{board_array[8]} "
+end
+
+display_board(board_array)
