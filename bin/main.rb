00f217695282bdbcace85f6f8226bdfd8165e3e5
ebeagusamuel <samuelebeagu@gmail.com>
2020-05-19 15:55:43 +0100

    Organize the code base into diffrent classes - The different methods were moved to their appropriate classes. - We added a counter to count for the number of wins each player has made at the end of the game.

--- bin/main.rb
+++ bin/main.rb
@@ -32,39 +32,17 @@ def start_game
   puts 'Let the game begin'
 end
 
-def player_turn(player_one, player_two, curr_board)
-  item_count = curr_board.integer_count
-
-  if item_count.even?
-    puts "#{player_two.name}, it is your turn..."
-    player_two
-  else
-    puts "#{player_one.name}, it is your turn..."
-    player_one
-  end
-end
-
-def player_move
-  puts 'Choose a number between 1 and 9...'
-  move = gets.chomp.to_i
-  if move.zero?
-    puts 'You chose an invalid number, please choose again ...'
-    player_move
-  else
-    move
-  end
-end
-
 def play_game(curr_board, player1, player2, player)
   loop do
-    player = player_turn(player1, player2, curr_board)
-    move = player_move
+    player = Player.player_turn(player1, player2, curr_board)
+    move = Player.player_move
     if Validators.validate_move?(curr_board.board, move)
       curr_board.update_board(move, player)
       curr_board.display_board
     else
       puts 'Invalid move. Please try again...'
-      player_move
+      curr_board.display_board
+      play_game(curr_board, player1, player2, player)
     end
     break if Validators.won?(curr_board.board, player) || Validators.draw?(curr_board.board, player)
   end
@@ -72,15 +50,23 @@ def play_game(curr_board, player1, player2, player)
 end
 
 def game_over(board, player1, player2, player)
-  puts "*****#{player.name} has won the game!*****" if Validators.won?(board, player)
+  if Validators.won?(board, player)
+    puts "*****#{player.name} has won the game!*****"
+    player.num_of_wins += 1
+  end
   puts 'The game is a draw!' if Validators.draw?(board, player)
   puts "If you want to play again press 'Y'"
   answer = gets.chomp.downcase
-  return puts 'Ending game now!' unless answer == 'y'
-  curr_board = Board.new
-  puts "Starting new game ..."
-  curr_board.display_board
-  play_game(curr_board, player1, player2, player)
+  if answer != 'y'
+    puts 'Ending game now!'
+    puts "#{player1.name} has won #{player1.num_of_wins} time(s)."
+    puts "#{player2.name} has won #{player2.num_of_wins} time(s)."
+  else
+    curr_board = Board.new
+    puts 'Starting new game ...'
+    curr_board.display_board
+    play_game(curr_board, player1, player2, player)
+  end
 end
 
 welcome
@@ -88,6 +74,6 @@ player1 = get_player1_name(user_symbols)
 player2 = get_player2_name(user_symbols)
 start_game
 curr_board = Board.new
-player = player_turn(player1, player2, curr_board)
+player = Player.player_turn(player1, player2, curr_board)
 curr_board.display_board
 play_game(curr_board, player1, player2, player)
