95877f623fdd71ee4b2c9b4f8b9fc54e7b4a18f0
Christopher Amanor <kriskit7@gmail.com>
2020-05-22 12:42:12 +0000

    Add Rspec test for update_board method in Board class

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,7 @@ class Board
   attr_accessor :board
 
   def initialize
-    # @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @board = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def integer_count
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,21 @@
 require_relative '../lib/board.rb'
 
 describe Board do
-    describe "#integer_count" do
-        it "returns true if return value is an integer or nil" do
-            board = Board.new
-            count = board.integer_count
-            expect(count).to be_a Integer || Nil
-        end
+  describe '#integer_count' do
+    it 'returns true if return value is an integer or nil' do
+      board = Board.new
+      count = board.integer_count
+      expect(count).to be_a Integer || Nil
     end
+  end
+
+  describe '#update_board' do
+    it 'should return true if board is updated' do
+      curr_board = Board.new
+      player = Player.new('chris', 'O')
+      player_move = 5
+      curr_board.update_board(player_move, player)
+      expect(curr_board.board[4]).to eql('O')
+    end
+  end
 end
