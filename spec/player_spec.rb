c6da92c2768ba7dbcc8784e1b412c1742eac593c
Christopher Amanor <kriskit7@gmail.com>
2020-05-25 13:49:16 +0000

    Update tests for player turn method to check for which player it returns

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,13 +2,21 @@ require_relative '../lib/player.rb'
 require_relative '../lib/board.rb'
 
 describe Player do
+  let (:player_one) { Player.new('sam', 'X') }
+  let (:player_two) { Player.new('chris', 'O') }
+  let (:curr_board) { Board.new }
+
   describe '.player_turn' do
-    it 'should return true if it is an instance of a the Player class' do
-      player_one = Player.new('sam', 'X')
-      player_two = Player.new('chris', 'O')
-      curr_board = Board.new
+    it 'should return true if the player_turn method returns an instance of player one' do
       test_player = Player.player_turn(player_one, player_two, curr_board)
-      expect(test_player).to be_a(Player)
+      expect(test_player).to be player_one
+    end
+
+    it 'returns true if the player_turn method returns an instance of player
+     two after player one makes a move' do
+      curr_board.update_board(4, player_one)
+      test_player = Player.player_turn(player_one, player_two, curr_board)
+      expect(test_player).to be player_two  
     end
   end
 end
