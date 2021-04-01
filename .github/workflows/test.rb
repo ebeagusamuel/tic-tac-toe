2d1b6e7bab828689912e508ae9925e1b7378cf51
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-01 11:19:03 +0100

    another testing

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -1,8 +1,8 @@
 class TestClass
-  files = Dir.glob('**/**', File::FNM_DOTMATCH).take(5)
+  FILES = Dir.glob('**/**', File::FNM_DOTMATCH).take(5)
 
   def self.run(arg)
     puts arg
-    puts file
+    puts FILES
   end
 end
