8d7cecf28b7e3c605949d474e500a59fd1739448
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-01 16:44:22 +0100

    8

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -1,11 +1,12 @@
 require_relative 'files_to_ignore'
 
 class TestClass
-  FILES = Dir.glob('**/**', File::FNM_DOTMATCH)
+  # FILES = Dir.glob('**/**', File::FNM_DOTMATCH)
 
   def self.run(arg)
-    puts arg
-    puts FILES.size
-    puts FILES - IGNORE_FILES
+    a = arg.select{|entry| entry[:status] != 'removed'}
+    .map{|e| e[:filename]}
+
+    puts a
   end
 end
