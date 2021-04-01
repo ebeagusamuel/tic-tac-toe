d0d66a0e28c051f6704456b0d951dd22cbbbc8a0
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-01 12:01:14 +0100

    Another testing again

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -1,9 +1,11 @@
+require_relative 'files_to_ignore'
+
 class TestClass
   FILES = Dir.glob('**/**', File::FNM_DOTMATCH)
 
   def self.run(arg)
     puts arg
     puts FILES.size
-    puts FILES
+    puts FILES - IGNORE_FILES
   end
 end
