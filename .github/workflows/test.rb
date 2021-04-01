9dd144043adcf100949c9b35172fc143775bdbb7
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-01 11:35:17 +0100

    Another testing

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -1,9 +1,9 @@
 class TestClass
-  FILES = Dir.glob('**/**', File::FNM_DOTMATCH)
+  # FILES = Dir.glob('**/**', File::FNM_DOTMATCH)
 
   def self.run(arg)
     puts arg
-    puts FILES.size
-    puts FILES
+    # puts FILES.size
+    puts File.read('lib/board.rb')
   end
 end
