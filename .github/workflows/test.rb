a8f9cf6ee01d60f19e39c63678eee3e95038d1ec
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-04 16:09:14 +0100

    rrrr

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -1,11 +1,11 @@
-require 'faraday'
+# require 'faraday'
 require 'json'
 
 class TestClass
   def self.run(pr_num, repo, token)
     # url = "https://api.github.com/repos/ebeagusamuel/tic-tac-toe/pulls/8/files"
     # response = Faraday.get(url, {"Accept" => "application/vnd.github.v3+json"})
-    # puts JSON.parse(response.body)
+    # JSON.parse(response.body)
     # puts arg
     puts pr_num
     puts repo
