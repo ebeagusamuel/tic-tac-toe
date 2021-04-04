3f12fbb12cdd6fa98e39ba25d5525c37b5142a4d
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-04 14:17:35 +0100

    eee

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -2,10 +2,13 @@ require 'faraday'
 require 'json'
 
 class TestClass
-  def self.run(arg)
-    url = "https://api.github.com/repos/ebeagusamuel/tic-tac-toe/pulls/8/files"
-    response = Faraday.get(url, {"Accept" => "application/vnd.github.v3+json"})
-    puts JSON.parse(response.body)
-    puts arg
+  def self.run(pr_num, repo, token)
+    # url = "https://api.github.com/repos/ebeagusamuel/tic-tac-toe/pulls/8/files"
+    # response = Faraday.get(url, {"Accept" => "application/vnd.github.v3+json"})
+    # puts JSON.parse(response.body)
+    # puts arg
+    puts pr_num
+    puts repo
+    puts token
   end
 end
