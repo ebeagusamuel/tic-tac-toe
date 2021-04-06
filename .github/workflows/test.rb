404cf218b65f8390a655f85d010c027272e54fd7
ebeagusamuel <samuelebeagu@gmail.com>
2021-04-06 03:15:31 +0100

    rrr

--- .github/workflows/test.rb
+++ .github/workflows/test.rb
@@ -3,29 +3,28 @@ require 'faraday'
 require 'json'
 
 class RulesMatchingZeroFiles
-  attr_reader :pr_num, :repo, :repo_files
+  attr_reader :pr_num, :repo, :token, :repo_files
 
-  def initialize(pr_num, repo)
+  def initialize(pr_num, repo, token)
     @pr_num = pr_num
     @repo = repo
+    @token = token
     @repo_files = Dir.glob('**/**', File::FNM_DOTMATCH)
   end
 
   def run
-    rules = rules_matching_zero_files(pr_num, repo)
+    rules = rules_matching_zero_files(pr_num, repo, token)
 
-    if rules.nil? || rules.none?
-      puts 0
-    else
-      puts 1
-    end
+    return if rules.nil? || rules.none?
+    
+    puts 'Hello'; exit 1
   end
 
   private
 
-  def rules_matching_zero_files(pr_num, repo)
-    modified = retrieve_pr_files(pr_num, repo, 'modified')
-    removed = retrieve_pr_files(pr_num, repo, 'removed')
+  def rules_matching_zero_files(pr_num, repo, token)
+    modified = retrieve_pr_files(pr_num, repo, token, 'added')
+    removed = retrieve_pr_files(pr_num, repo, token, 'removed')
     parsed_codeowners = parse_codeowners
 
     if modified.include?('.github/CODEOWNERS')
@@ -36,7 +35,7 @@ class RulesMatchingZeroFiles
       rules_matching_removed_files = parsed_codeowners.keys.each_with_object([]) do |rule, result|
         result << rule if removed.any?{|file| match?(rule, file)}
       end
-      p rules_matching_removed_files
+
       rules_matching_removed_files.each_with_object([]) do |rule, result|
         result << rule unless repo_files.any?{|file| match?(rule, file)}
       end
@@ -53,7 +52,6 @@ class RulesMatchingZeroFiles
       teams = parts.drop(1).to_a
       h[pattern] = teams if pattern
     end
-    p h
     h
   end
 
@@ -62,20 +60,19 @@ class RulesMatchingZeroFiles
     pathspec_pattern.match(dir)
   end
 
-  def retrieve_pr_files(pr_num, repo, status)
+  def retrieve_pr_files(pr_num, repo, token, status)
     url = "https://api.github.com/repos/#{repo}/pulls/#{pr_num}/files"
-    response = Faraday.get(
+    response = Faraday.new(
       url,
-       {
-         "Accept" => "application/vnd.github.v3+json",
-        }
-      )
+      headers: { 
+      "Accept" => "application/vnd.github.v3+json",
+      "Authorization" => "token #{token}"
+      }
+    ).get        
     files = JSON.parse(response.body)
     
-    pp files.select{|file| file['status'] == status}.map{|file| file['filename']}
-
     files.select{|file| file['status'] == status}.map{|file| file['filename']}
   end
 end
 
-RulesMatchingZeroFiles.new(9, 'ebeagusamuel/tic-tac-toe').run
+# RulesMatchingZeroFiles.new(1, 'ebeagusamuel/samAndKayode', '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').run
